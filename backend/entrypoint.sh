110a18e534b9dd77ca62f6119bfcff7e17aecb38
Raymond Kipkorir <raykipkorir02@gmail.com>
2023-06-14 16:46:53 +0300

    modify dockerfile and entrypoint.sh

---
+++ backend/entrypoint.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+set -e
+set -x
+
+cd backend
+python3 manage.py makemigrations --no-input
+python3 manage.py migrate --no-input
+python3 manage.py collectstatic --no-input
+
+gunicorn core.wsgi
