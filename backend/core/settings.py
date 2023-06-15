dfda357279f63dcb7b5eed8cbea2a34e2f836c2a
Raymond Kipkorir <raykipkorir02@gmail.com>
2023-06-15 05:17:27 +0300

    add static_root

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 from decouple import config, Csv
 from dj_database_url import parse as db_url
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,6 +134,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
