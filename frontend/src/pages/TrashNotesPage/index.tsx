a6fe8fa4525e59758f8a387740294dc23b58b14c
Raymond Kipkorir <raykipkorir02@gmail.com>
2023-06-15 13:13:14 +0300

    add loading spinners

--- frontend/src/pages/TrashNotesPage/index.tsx
+++ frontend/src/pages/TrashNotesPage/index.tsx
@@ -1,3 +1,4 @@
+import CircularProgress from "@mui/material/CircularProgress";
 import { useEffect, useState } from "react";
 import noteService from "../../services/notes.services";
 import NotesDetail from "../AllNotesPage/NotesDetail";
@@ -5,10 +6,13 @@ import { Note } from "../AllNotesPage/NotesListType";
 
 function index() {
   const [notes, setNotes] = useState<[]>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const retrieveTrashNotes = async () => {
+      setIsLoading(true);
       const response = await noteService.getTrashNotes();
+      setIsLoading(false);
       if (response.status === 200) {
         const data = response.data;
         if (data.length >= 1) {
@@ -21,7 +25,9 @@ function index() {
   return (
     <div>
       <p>Notes thrown in the trash</p>
-      {notes ? (
+      {isLoading ? (
+        <CircularProgress />
+      ) : notes ? (
         notes.map((note: Note) => <NotesDetail key={note.id} note={note} />)
       ) : (
         <p>No notes in the trash</p>
